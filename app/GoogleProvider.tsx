b9d69680265c904b8127597ca0d406513ee80669
aonoexercist <xercisthegreat@gmail.com>
2026-03-31 16:42:06 +0800

    Add GoogleOneTapButton to GoogleProvider for enhanced authentication

--- app/GoogleProvider.tsx
+++ app/GoogleProvider.tsx
@@ -2,12 +2,14 @@
 
 import React from "react"
 import { GoogleOAuthProvider } from "@react-oauth/google"
+import GoogleOneTapButton from "./components/GoogleOneTapButton"
 
 const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""
 
 export default function GoogleProvider({ children }: { children: React.ReactNode }) {
   return (
     <GoogleOAuthProvider clientId={clientId}>
+      <GoogleOneTapButton />
       {children}
     </GoogleOAuthProvider>
   )
