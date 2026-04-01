458bd6d12f8c33a0f0d474c00571baa82da6cb9d
aonoexercist <xercisthegreat@gmail.com>
2026-04-01 08:49:01 +0800

    Update GoogleProvider to conditionally render GoogleOneTapButton based on pathname

--- app/GoogleProvider.tsx
+++ app/GoogleProvider.tsx
@@ -3,13 +3,19 @@
 import React from "react"
 import { GoogleOAuthProvider } from "@react-oauth/google"
 import GoogleOneTapButton from "./components/GoogleOneTapButton"
+import { usePathname } from "next/navigation"
 
 const clientId = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID || ""
 
+const NO_ONE_TAP_PATHS = ["/login", "/register"]
+
 export default function GoogleProvider({ children }: { children: React.ReactNode }) {
+  const pathname = usePathname()
+  const showOneTap = !NO_ONE_TAP_PATHS.includes(pathname)
+
   return (
     <GoogleOAuthProvider clientId={clientId}>
-      <GoogleOneTapButton />
+      {showOneTap && <GoogleOneTapButton />}
       {children}
     </GoogleOAuthProvider>
   )
