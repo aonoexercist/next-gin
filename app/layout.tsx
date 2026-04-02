807f42cbed22dec227e859e47c4493b4841c2505
aonoexercist <xercisthegreat@gmail.com>
2026-04-02 15:14:10 +0800

    Remove GoogleProvider wrapper from RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import GoogleProvider from "./GoogleProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +28,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <GoogleProvider>{children}</GoogleProvider>
+        {children}
       </body>
     </html>
   );
