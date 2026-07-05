6180a6d4e11e3454d6b23885d6a42cd3b3b1236b
aonoexercist <xercisthegreat@gmail.com>
2026-07-05 15:54:56 +0800

    fix: restructure DashboardLayout to include Sidebar and improve layout

--- app/(locale)/dashboard/layout.tsx
+++ app/(locale)/dashboard/layout.tsx
@@ -1,10 +1,15 @@
 import NavBar from "@/components/NavBar"
+import Sidebar from "@/components/Sidebar"
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
-      <NavBar />
-      {children}
-    </>
+    <div className="flex flex-col md:flex-row flex-1">
+      <Sidebar />
+
+      <section className="flex-1 min-w-0">
+        <NavBar />
+        {children}
+      </section>
+    </div>
   )
 }
